539ca6efa0b96e24be9674635d31d9f15a86b6fd
root <root@ip-172-31-16-115.eu-west-2.compute.internal>
2021-05-11 09:22:11 +0000

    new version of werserver

--- cookbooks/apache/recipes/webserver.rb
+++ cookbooks/apache/recipes/webserver.rb
@@ -1,11 +1,14 @@
 package 'httpd' do
   action :install
 end
-service 'httpd' do
-  action :start
-end
+
 file '/var/www/html/index.html' do
-  content 'New webserver for cookbook testing'
+  content "<h1>New webserver for cookbook testing</h1>
+  <h2>IPADDRESS: #{node['ipaddress'] } </h2>
+  <h2>HOSTNAME: #{node['hostname'] } </h2>"
+  
+  
+
   owner 'root'
   group 'root'
   mode '0755'
